0ff74ed911ba5ceb8b036e3c30c9625400c1deae
MarloesBijkerk <marloesbijkerk@gmail.com>
2019-11-18 13:54:19 +0100

    PlaylistControllerTest
    PlaylistController unit testen klaar.

--- Ritmo/Ritmo.UnitTests/PlayListControllerTest.cs
+++ Ritmo/Ritmo.UnitTests/PlayListControllerTest.cs
@@ -8,9 +8,9 @@ namespace Ritmo.UnitTests
     [TestClass]
     public class PlayListControllerTest
     {
-        //Method: AddTrack(Track track)
+        //Method: AddTrack(Track track) - 
         [TestMethod]
-        public void AddTrackToPlaylist_ReturnTrue()
+        public void Add_SuccessScenario_ReturnsTrue()
         {
             //Arrange
             bool result = false;
@@ -23,9 +23,22 @@ namespace Ritmo.UnitTests
             Assert.AreEqual(result, true);
         }
 
+        //Method: AddTrack(Track track)
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Add_TrackALreadyExists_ReturnsException()
+        {
+            //Arrange
+            Track track = new Track();
+            PlaylistController playlistController = new PlaylistController("Playlist");
+            //Act / Assert
+            playlistController.AddTrack(track);
+            playlistController.AddTrack(track);
+        }
+
         //Method: RemoveTrack(Track track)
         [TestMethod]
-        public void RemoveTrackFromPlaylist_ReturnFalse()
+        public void Remove_SuccessScenario_ReturnsFalse()
         {
             //Arrange
             bool result = false;
@@ -37,6 +50,33 @@ namespace Ritmo.UnitTests
             result = playlistController.Playlist.Tracks.Contains(track);
             //Assert
             Assert.AreEqual(result, false);
+        }
+
+        //Method: RemoveTrack(Track track)
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Remove_TrackDoesNotExists_ReturnsException()
+        {
+            //Arrange
+            Track track = new Track();
+            PlaylistController playlistController = new PlaylistController("Playlist");
+            //Act / Assert
+            playlistController.RemoveTrack(track);
+            playlistController.Playlist.Tracks.Contains(track);
+        }
+
+        //Method: SetName(string name)
+        [TestMethod]
+        public void SetName_ChangeName_ReturnsName()
+        {
+            //Arrange
+            PlaylistController playlistController = new PlaylistController("Before");
+            string result;
+            //Act
+            playlistController.SetName("After");
+            result = playlistController.Playlist.Name;
+            //Assert
+            Assert.AreEqual(result, "After");
         }
     }
 }
