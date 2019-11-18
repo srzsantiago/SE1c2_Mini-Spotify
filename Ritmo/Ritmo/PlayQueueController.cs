521709d7a5de08c7f539db8b6bd40f6a037379d3
srzsantiago <s1131631@student.windesheim.nl>
2019-11-18 10:29:22 +0100

    Comments in playqueue controller

--- Ritmo/Ritmo/PlayQueueController.cs
+++ Ritmo/Ritmo/PlayQueueController.cs
@@ -10,22 +10,23 @@ namespace Ritmo
     {
         public PlayQueue PlayQueue { get; set; }
 
-        public PlayQueueController()
+        public PlayQueueController()//Constructor that initializate a new playQueue and link this to the playqueuecontroller
         {
             this.PlayQueue = new PlayQueue(); 
         }
 
-        public void PlayTrack(Track track) {
+        public void PlayTrack(Track track)//Set the currentTrack with a single track choiced by the user
+        {
             PlayQueue.CurrentTrack = track;
         }
-        public void PlayTrack(Track track, TrackList trackList)
+        public void PlayTrack(Track track, TrackList trackList)//set the currentTrack with a track from a tracklist(playlist/album) choised by the user and at the whole list to the WaitingList
         {
             PlayTrack(track);
             SetTrackWatingList(trackList);
         }
         public void ResumeTrack() { }
         public void PauseTrack() { }
-        public void NextTrack() {
+        public void NextTrack() {//Set the CurrentTrack as the next track
             if (PlayQueue.TrackQueueHasSongs())
                 PlayQueue.CurrentTrack = PlayQueue.TrackQueue.Dequeue();
             else
@@ -33,15 +34,15 @@ namespace Ritmo
 
             //what will happen if there is no playlist linked to the WaitingList
         }
-        public void PreviousTrack() {
+        public void PreviousTrack() {//Set the CurrentTrack as the previous track
             if(PlayQueue.TrackWaitingList.Contains(PlayQueue.CurrentTrack))
                 PlayQueue.CurrentTrack = PlayQueue.TrackWaitingList.Find(PlayQueue.CurrentTrack).Previous.Value;
             //there is some logica missing here
         }
-        public void AddTrack(Track track) {
+        public void AddTrack(Track track) {//Add track to the queue
             PlayQueue.TrackQueue.Enqueue(track);
         }
-        public void RemoveTrackFromQueue(Track track, int index) {
+        public void RemoveTrackFromQueue(Track track, int index) {//remove track from the queue
             int count = 0;
             Queue<Track> helpStack = new Queue<Track>();
             
@@ -66,20 +67,20 @@ namespace Ritmo
         }
 
 
-        public void RemoveTrackFromWaitingList(Track track)
+        public void RemoveTrackFromWaitingList(Track track)//Remove track from the waitinglist(Copy of a tracklist)
         {
             PlayQueue.TrackWaitingList.Remove(track);     
         }
-        public void SetTrackWatingList(TrackList trackList) {
+        public void SetTrackWatingList(TrackList trackList) {//Set the waitingList with a tracklist(playlist/album)
             PlayQueue.TrackWaitingList = trackList.Tracks;
         }
         public void ShuffleTrackWaitingList() {}
-        public void RepeatTrackWaitingList() { } //kan je een queue repeaten?
-        public void RepeatTrack() { } //kan je een queue repeaten?
-        public void SetVolume(double volume) {
+        public void RepeatTrackWaitingList() { } //Repeat the whole waitingList (the queue can't be repeated)
+        public void RepeatTrack() { } //Repeat the currenttrack while its active.
+        public void SetVolume(double volume) {//Set the volume to a given value
             PlayQueue.CurrentVolume = volume;
         }
-        public void SetMute() {
+        public void SetMute() { //Turn the Mutemode on en off.
             PlayQueue.IsMute = !PlayQueue.IsMute;
         }
     }
