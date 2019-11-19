63bf388d4fb7c9fdc472bc083da1d5043ff15a71
MarloesBijkerk <marloesbijkerk@gmail.com>
2019-11-19 14:57:52 +0100

    Unit testen

--- Ritmo/Ritmo.UnitTests/PlayQueueControllerTest.cs
+++ Ritmo/Ritmo.UnitTests/PlayQueueControllerTest.cs
@@ -22,9 +22,21 @@ namespace Ritmo.UnitTests
             Assert.AreEqual(result, track);
         }
 
-        //Method: PlayTrack(Track track, TrackList trackList) - set the currentTrack with a track from a tracklist(playlist/album) chosen by the user and at the whole list to the WaitingList
-        //
+        //Method: PlayTrack(Track track, TrackList trackList) - 
+        //set the currentTrack with a track from a tracklist(playlist/album) chosen by the user and at the whole playlist will be added to the waitinglist
+        //Success scenario.
         [TestMethod]
-        public void PlayTrack_SuccessScenario_
+        public void PlayTrack_SuccessScenario_AddsPlaylistToWaitingList()
+        {
+            //Arrange
+            PlayQueueController playQueueController = new PlayQueueController();
+            Track track = new Track();
+            Playlist trackList = new Playlist("New");
+            //Act
+            playQueueController.PlayTrack(track, trackList);
+            var result = playQueueController.playQueue.CurrentTrack;
+            //Assert
+            Assert.AreEqual(result, track);
+        }
     }
 }
