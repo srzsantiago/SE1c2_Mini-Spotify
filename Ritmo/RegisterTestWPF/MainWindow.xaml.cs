b00fdbf34dc1738728f36437a57846eaf1860b42
Susan Wilms <susan.wilms@hotmail.com>
2019-11-21 13:37:03 +0100

    register screen

---
+++ Ritmo/RegisterTestWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegisterTestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Artistbutton_Click(object sender, RoutedEventArgs e)
+        {
+            var currentwindow = new Artist();
+            currentwindow.Show();
+        }
+
+        private void Cancelbutton_Click(object sender, RoutedEventArgs e)
+        {
+            loginscherm.MainWindow loginscherm = new loginscherm.MainWindow();
+            loginscherm.Show();
+            this.Close();
+        }
+
+        private void Createbutton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
