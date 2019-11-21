91abce184433bfca78452d145e991bb5ebf4d495
Susan Wilms <susan.wilms@hotmail.com>
2019-11-21 11:37:35 +0100

    updated Login and Register
    Added potential sql query's to login and finished first version of register

--- Ritmo/Ritmo/Login.cs
+++ Ritmo/Ritmo/Login.cs
@@ -12,16 +12,41 @@ namespace Ritmo
         public bool loggedin = false;
         AccessLevel access { get; set; }
 
-        // moet uit db gehaald worden 
-        public string email = "gebruiker1@email.com";
+        // Hard-coded credentials, uncomment code in 'Login' to use database credentials. 
+        public string email = "gebruiker1@mail.com";
         public string password = "gebruiker1";
         public AccessLevel accessdb = AccessLevel.admin;
 
         // login function
         public Login(string email, string password)
         {
-            if(this.email == email)
+            /* SqlCommand command;
+             * SqlDataReader dataReader;
+             * String sql,Output = "";
+             * 
+             * GET EMAILADRESSES FROM DATABASE
+             * sql = "SELECT mail FROM users WHERE mail = email";
+             * command = new SqlCommand (sql, conn); // conn contains the connection with the database. 
+             * dataReader = command.ExecuteReader();
+             * 
+             * while (dataReader.Read()){
+             *  Output = Output + dataReader.GetValue(0)
+             * }
+             */
+
+            // if(Output == email)
+            if (this.email == email)
             {
+                /* sql = "SELECT password FROM users WHERE mail = email";
+                 * command = new SqlCommand (sql, conn); // conn contains the connection with the database. 
+                 * dataReader = command.ExecuteReader();
+                 * 
+                 * while (dataReader.Read()){
+                 *  Output = Output + dataReader.GetValue(0)
+                 * }
+                 */
+
+                // if (output != password)
                 if (this.password != password)
                 {
                     loggedin = false;
@@ -30,6 +55,16 @@ namespace Ritmo
                 {
                     this.email = email;
                     this.password = password;
+                    /* sql = "SELECT access FROM users WHERE mail = email";
+                    * command = new SqlCommand (sql, conn); // conn contains the connection with the database. 
+                    * dataReader = command.ExecuteReader();
+                    * 
+                    * while (dataReader.Read()){
+                    *  Output = Output + dataReader.GetValue(0)
+                    * }
+                    */
+
+                    //this.access = output;
                     this.access = accessdb;
                     loggedin = true;
                     // create new user with the loggedin bool and the access enum value
---
+++ Ritmo/Ritmo/Register.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ritmo
+{
+    class Register
+    {
+        /* SqlCommand command;
+         * SqlDataReader dataReader;
+         * String sql,Output = "";
+         */
+        private string  Name;
+        private string  Email;
+        private string  Password;
+        private bool    Artist;
+
+        public string existingTestMail = "email@example.com"; 
+
+        public Register(string name, string email, string password, string confirmpw)
+        {
+            if(password == confirmpw)
+            {
+                /*GET EMAILADRESSES FROM DATABASE
+                * sql = "SELECT mail FROM users WHERE mail = email";
+                * command = new SqlCommand(sql, conn); // conn contains the connection with the database. 
+                * dataReader = command.ExecuteReader();
+                *
+                * while (dataReader.Read())
+                * {
+                *   Output = Output + dataReader.GetValue(0)
+                * }
+                */
+
+                //if (email != Output)
+                if(email != existingTestMail)
+                {
+                    Name        = name;
+                    Email       = email;
+                    Password    = password;
+                    Artist      = false;
+                    return "Your account has been successfully created";
+                }
+                else
+                {
+                    return "This email already exists";
+                }
+            }
+            else
+            {
+                return "The passwords do not match";
+            }
+        }
+
+        public Register(string email)
+        {
+            /*GET EMAILADRESSES FROM DATABASE
+             * sql = "SELECT mail FROM users WHERE mail = email";
+             * command = new SqlCommand(sql, conn); // conn contains the connection with the database. 
+             * dataReader = command.ExecuteReader();
+             *
+             * while (dataReader.Read())
+             * {
+             *   Output = Output + dataReader.GetValue(0)
+             * }
+             */
+
+            //if (email != Output)
+            if (email != existingTestMail)
+            {
+                Email   = email;
+                Artist  = true;
+            }
+        }
+    }
+}
