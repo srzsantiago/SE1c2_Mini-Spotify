27c40d610d00ee0f895f84676ce30716e67954b1
SteelpanKooij <44843993+SteelpanKooij@users.noreply.github.com>
2019-11-26 17:59:09 +0100

    Changed MainWindow code-behind and XAML to MainWindowViewModel

---
+++ Ritmo/Ritmo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,192 @@
+﻿using Caliburn.Micro;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Ritmo;
+
+namespace Ritmo.ViewModels
+{
+    public class MainWindowViewModel : Screen
+    {
+        PlaylistController PlaylistController = new PlaylistController("TestPlaylist");
+        PlayQueueController PlayQueueController = new PlayQueueController();
+
+        #region Commands
+        public ICommand ChangeViewModelCommand { get; set; }
+        #endregion
+
+        #region ViewModel attributes
+        private Screen _currentViewModel = new HomeViewModel();
+        public Screen HomeViewModel { get; set; } = new HomeViewModel();
+        public Screen SearchViewModel { get; set; } = new SearchViewModel();
+        public Screen CategoriesViewModel { get; set; } = new CategoriesViewModel();
+        public Screen FollowingViewModel { get; set; } = new FollowingViewModel();
+        public Screen AllPlaylistsViewModel { get; set; } 
+        public Screen MyQueueViewModel { get; set; } = new MyQueueViewModel();
+
+        public Screen CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                NotifyOfPropertyChange(() => CurrentViewModel);
+            }
+        }
+        #endregion
+
+        #region CurrentTrack commands and attributes
+        public ICommand PlayTrackCommand { get; set; }
+        public ICommand NextTrackCommand { get; set; }
+        public ICommand PrevTrackCommand { get; set; }
+
+        private MediaElement _currentTrackElement = new MediaElement() { LoadedBehavior = MediaState.Manual};
+        private Uri _currentTrackSource; //Unused
+        private double _currentTrackVolume = 0.5;
+        private Uri _playButtonIcon = new Uri("/ImageResources/playbutton.png", UriKind.RelativeOrAbsolute);
+
+        public MediaElement CurrentTrackElement
+        {
+            get { return _currentTrackElement; }
+            set { _currentTrackElement = value; }
+        }
+        public Uri CurrentTrackSource
+        {
+            get { return _currentTrackSource; }
+            set { _currentTrackSource = value; }
+        } //Unused
+        public double CurrentTrackVolume
+        {
+            get { return _currentTrackVolume; }
+            set 
+            { 
+                _currentTrackVolume = value;
+                ChangeMediaVolume(value);
+            }
+        }
+        public Uri PlayButtonIcon { get { return _playButtonIcon; } set { _playButtonIcon = value; NotifyOfPropertyChange(); } }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            InitializeCommands();
+            InitializeViewModels();
+            InitializeCurrentTrackElement();
+
+            TestTrackMethod();
+        }
+
+        //The methods that control or interact with the CurrentTrackElement
+        #region CurrentTrackElement Methods
+        public void PlayTrack()
+        {
+            if (CurrentTrackElement.IsLoaded)
+            {
+                CurrentTrackElement.Play();
+                PlayButtonIcon = new Uri(@"\ImageResources\pausebutton.png", UriKind.Relative);
+            }
+        }
+
+        //Pauses track and updates play/pausebutton
+        public void PauseTrack()
+        {
+            if (PlayQueueController.PQ.TrackWaitingListEnded)
+            {
+                CurrentTrackElement.Pause();
+                PlayButtonIcon = new Uri(@"\ImageResources\playbutton.png", UriKind.Relative);
+            }
+        }
+
+        //Changes to next track, set CurrentTrackElement and plays track.
+        public void NextTrack()
+        {
+            PlayQueueController.NextTrack();
+            CurrentTrackElement.Source = PlayQueueController.PQ.CurrentTrack.AudioFile;
+            if (!PlayQueueController.PQ.TrackWaitingListEnded)
+            {
+                PlayTrack();
+            }
+        }
+
+        ////Changes to the previous track and set CurrentTrackElement
+        public void PrevTrack()
+        {
+            PlayQueueController.PreviousTrack();
+            CurrentTrackElement.Source = PlayQueueController.PQ.CurrentTrack.AudioFile;
+            PlayTrack();
+        }
+
+        //Runs when the track has ended. The next track will be loaded and played.
+        //It's assigned to CurrentTrackElement.MediaEnded in the MainWindowViewModel constructor
+        //If the playQueue has played all tracks, CurrentTrack will be set to the first Track in TrackWaitingList and the audio will be paused.
+        public void Track_Ended(Object sender, EventArgs e)
+        {
+            NextTrack();
+            PauseTrack();
+        }
+
+        //Changes volume based on slider. 0 is muted and 1 is the highest volume.
+        public void ChangeMediaVolume(double VolumeSliderValue)
+        {
+            CurrentTrackElement.Volume = VolumeSliderValue; // gets the slider value and puts it as volume
+        }
+
+        #endregion
+       
+        //Initialize-methods that are used in the constructor of MainWindowViewModel
+        #region Initializer methods
+        public void InitializeCommands()
+        {
+            ChangeViewModelCommand = new RelayCommand<Screen>(ChangeViewModel); //Sets the command to the corresponding method
+            PlayTrackCommand = new RelayCommand(PlayTrack);
+            NextTrackCommand = new RelayCommand(NextTrack);
+            PrevTrackCommand = new RelayCommand(PrevTrack);
+        }
+        public void InitializeViewModels()
+        {
+            AllPlaylistsViewModel = new AllPlaylistsViewModel(this);
+        }
+        public void InitializeCurrentTrackElement()
+        {
+            //CurrentTrackElement.Source = CurrentTrackSource;
+            CurrentTrackElement.MediaEnded += Track_Ended;
+            CurrentTrackElement.Volume = CurrentTrackVolume;
+        }
+        #endregion
+
+        //Changes CurrentViewModel and sets the frame
+        public void ChangeViewModel(Screen ViewModel)
+        {
+            this.CurrentViewModel = ViewModel;
+        }
+
+        public void TestTrackMethod()
+        {
+            //Een test playlist
+            //Track testTrack1 = new Track() { AudioFile = new Uri(@"C:\RingtoneUnatco.mp3") };
+            Track testTrack1 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\RingtoneUnatco.mp3") };
+            Track testTrack2 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\RingtoneRoundabout.mp3") };
+            Track testTrack3 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\Powerup2.wav") };
+            Track testTrack4 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\Powerup1.wav") };
+            PlaylistController.AddTrack(testTrack1);
+            PlaylistController.AddTrack(testTrack2);
+            PlaylistController.AddTrack(testTrack3);
+            PlaylistController.AddTrack(testTrack4);
+
+            //Speelt track en zet playlist in wachtrij
+            PlayQueueController.PlayTrack(PlaylistController.Playlist.Tracks.First.Value, PlaylistController.Playlist);
+
+            //Zet de CurrentTrack als audio die afgespeeld wordt
+            CurrentTrackElement.Source = PlayQueueController.PQ.CurrentTrack.AudioFile;
+
+        }
+    }
+}
--- Ritmo/Ritmo/Views/MainWindowView.xaml.cs
+++ Ritmo/Ritmo/Views/MainWindowView.xaml.cs
@@ -25,120 +25,103 @@ namespace Ritmo.Views
     /// </summary>
     public partial class MainWindowView : Window
     {
-        PlayQueueController playQueueController = new PlayQueueController();
-        PlaylistController playlistController = new PlaylistController("TestPlaylist");
+        //PlayQueueController playQueueController = new PlayQueueController();
+        //PlaylistController playlistController = new PlaylistController("TestPlaylist");
 
-        public MainWindowView()
-        {
-            InitializeComponent();
-            CurrentTrackElement.LoadedBehavior = MediaState.Manual;
-            CurrentTrackElement.UnloadedBehavior = MediaState.Pause;
-            CurrentTrackElement.MediaEnded += Track_Ended;
+        //public MainWindowView()
+        //{
+        //    InitializeComponent();
+        //    CurrentTrackElement.LoadedBehavior = MediaState.Manual;
+        //    CurrentTrackElement.UnloadedBehavior = MediaState.Pause;
+            //CurrentTrackElement.MediaEnded += Track_Ended;
 
+        //    //TestTrackMethod();
+        //}
 
+        ////Methode om de logica te testen. Dit zijn uiteindelijk de stappen die de gebruiker zelf moet zetten in de GUI
+        //public void TestTrackMethod()
+        //{
+            
+        //}
 
-            TestTrackMethod();
-        }
+        ////Plays track and updates play/pausebutton
+        //public void PlayTrack()
+        //{
+        //    if (CurrentTrackElement.IsLoaded)
+        //    {
+        //        CurrentTrackElement.Play();
+        //        PlayButtonIcon.Source = new BitmapImage(new Uri(@"\ImageResources\pausebutton.png", UriKind.Relative));
+        //    }
 
-        //Methode om de logica te testen. Dit zijn uiteindelijk de stappen die de gebruiker zelf moet zetten in de GUI
-        public void TestTrackMethod()
-        {
-            //Een test playlist
-            Track testTrack1 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\RingtoneUnatco.mp3") };
-            Track testTrack2 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\RingtoneRoundabout.mp3") };
-            Track testTrack3 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\Powerup2.wav") };
-            Track testTrack4 = new Track() { AudioFile = new Uri(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\TestFiles\Powerup1.wav") };
-            playlistController.AddTrack(testTrack1);
-            playlistController.AddTrack(testTrack2);
-            playlistController.AddTrack(testTrack3);
-            playlistController.AddTrack(testTrack4);
+        //}
 
-            //Speelt track en zet playlist in wachtrij
-            playQueueController.PlayTrack(playlistController.Playlist.Tracks.First.Value, playlistController.Playlist);
+        ////Changes to next track, set CurrentTrackElement and plays track.
+        //public void PlayNextTrack()
+        //{
+        //    playQueueController.NextTrack();
+        //    CurrentTrackElement.Source = playQueueController.PQ.CurrentTrack.AudioFile;
+        //    if (!playQueueController.PQ.TrackWaitingListEnded)
+        //    {
+        //        PlayTrack();
+        //    }
+        //}
 
-            //Zet de CurrentTrack als audio die afgespeeld wordt
-            CurrentTrackElement.Source = playQueueController.PQ.CurrentTrack.AudioFile;
+        ////Changes to the previous track and set CurrentTrackElement
+        //public void PlayPreviousTrack()
+        //{
+        //    playQueueController.PreviousTrack();
+        //    CurrentTrackElement.Source = playQueueController.PQ.CurrentTrack.AudioFile;
+        //    PlayTrack();
+        //}
 
-        }
+        ////Pauses track and updates play/pausebutton
+        //public void PauseTrack()
+        //{
+        //    if (playQueueController.PQ.TrackWaitingListEnded)
+        //    {
+        //        CurrentTrackElement.Pause();
+        //        PlayButtonIcon.Source = new BitmapImage(new Uri(@"\ImageResources\playbutton.png", UriKind.Relative));
+        //    }
+        //}
 
-        //Plays track and updates play/pausebutton
-        public void PlayTrack()
-        {
-            if (CurrentTrackElement.IsLoaded)
-            {
-                CurrentTrackElement.Play();
-                PlayButtonIcon.Source = new BitmapImage(new Uri(@"\ImageResources\pausebutton.png", UriKind.Relative));
-            }
+        //#region Events
 
-        }
+        ////Runs when the track has ended. The next track will be loaded and played.
+        ////It's assigned to CurrentTrackElement.MediaEnded in the MainWindow constructor
+        ////If the playQueue has played all tracks, CurrentTrack will be set to the first Track in TrackWaitingList and the audio will be paused.
+        //public void Track_Ended(Object sender, EventArgs e)
+        //{
+        //    PlayNextTrack();
+        //    PauseTrack();
+        //}
 
-        //Changes to next track, set CurrentTrackElement and plays track.
-        public void PlayNextTrack()
-        {
-            playQueueController.NextTrack();
-            CurrentTrackElement.Source = playQueueController.PQ.CurrentTrack.AudioFile;
-            if (!playQueueController.PQ.TrackWaitingListEnded)
-            {
-                PlayTrack();
-            }
-        }
+        //private void Play_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    PlayTrack();
+        //}
 
-        //Changes to the previous track and set CurrentTrackElement
-        public void PlayPreviousTrack()
-        {
-            playQueueController.PreviousTrack();
-            CurrentTrackElement.Source = playQueueController.PQ.CurrentTrack.AudioFile;
-            PlayTrack();
-        }
+        //private void Next_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    PlayNextTrack();
+        //    PauseTrack();
+        //}
 
-        //Pauses track and updates play/pausebutton
-        public void PauseTrack()
-        {
-            if (playQueueController.PQ.TrackWaitingListEnded)
-            {
-                CurrentTrackElement.Pause();
-                PlayButtonIcon.Source = new BitmapImage(new Uri(@"\ImageResources\playbutton.png", UriKind.Relative));
-            }
-        }
+        //private void Prev_Clicked(object sender, RoutedEventArgs e)
+        //{
+        //    PlayPreviousTrack();
+        //}
 
-        #region Events
-
-        //Runs when the track has ended. The next track will be loaded and played.
-        //It's assigned to CurrentTrackElement.MediaEnded in the MainWindow constructor
-        //If the playQueue has played all tracks, CurrentTrack will be set to the first Track in TrackWaitingList and the audio will be paused.
-        public void Track_Ended(Object sender, EventArgs e)
-        {
-            PlayNextTrack();
-            PauseTrack();
-        }
-
-        private void Play_Clicked(object sender, RoutedEventArgs e)
-        {
-            PlayTrack();
-        }
-
-        private void Next_Clicked(object sender, RoutedEventArgs e)
-        {
-            PlayNextTrack();
-            PauseTrack();
-        }
-
-        private void Prev_Clicked(object sender, RoutedEventArgs e)
-        {
-            PlayPreviousTrack();
-        }
-
-        #endregion
+        //#endregion
 
 
-        // test tristan volume
+        //// test tristan volume
 
-        // changes volume based on slidebar, 0 is muted and 1 is the highest volume.
-        private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> args)
-        {
-            CurrentTrackElement.Volume = (double)volumeSlider.Value; // gets the slider value and puts it as volume
+        //// changes volume based on slidebar, 0 is muted and 1 is the highest volume.
+        //private void ChangeMediaVolume(object sender, RoutedPropertyChangedEventArgs<double> args)
+        //{
+        //    //CurrentTrackElement.Volume = (double)volumeSlider.Value; // gets the slider value and puts it as volume
 
-        }
+        //}
 
 
 
